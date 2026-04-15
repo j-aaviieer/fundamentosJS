f03f5bd087370a2d163cf8936bc392b0e48b8157
j-aaviieer <266820266+j-aaviieer@users.noreply.github.com>
2026-04-15 08:18:26 -0400

    asd

---
+++ 01_introduccion/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexion exitosa con js externo...");
+
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+
+console.log(saludo + "para los " + destinatario);
+
+let nombre = prompt("Ingrese su nombre: ");
+
+console.log("Hola " + nombre);
---
+++ 02_tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,7 @@
+console.log("Conexion con js correcta...");
+
+const nombre = "Javier";
+const apellido = "Colque";
+
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
+
